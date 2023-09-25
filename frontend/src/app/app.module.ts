438bf27c2ec357dd9a42096c1e5658cd8686189b
Hsuehkuan Lu <hsuehkuan.peter.lu@gmail.com>
2023-09-25 23:13:37 +0800

    Prettier all frontend files.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TopBarComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
