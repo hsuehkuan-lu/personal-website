9e5c236a9ccc2fa81a436edd18efd0cd5d20e2c8
Hsuehkuan Lu <hsuehkuan.peter.lu@gmail.com>
2024-07-26 23:23:07 +0800

    Finish first draft of my website.

--- frontend/src/app/constant.ts
+++ frontend/src/app/constant.ts
@@ -11,4 +11,10 @@ export abstract class Constants {
     this.WORK_PAGE,
     this.CONTACT_PAGE,
   ];
+  static readonly INSTAGRAM_LINK = 'https://www.instagram.com/lhkuan_/';
+  static readonly LINKEDIN_LINK =
+    'https://www.linkedin.com/in/hsuehkuan-lu-180407/';
+  static readonly GITHUB_LINK = 'https://github.com/hsuehkuan-lu';
+  static readonly MEDIUM_LINK = 'https://medium.com/@hsuehkuan.peter.lu';
+  static readonly MAILTO_LINK = 'mailto:hsuehkuanlu@gmail.com';
 }
